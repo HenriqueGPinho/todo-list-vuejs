9d2808e2471f11894155b30eadb34b1ba42718b3
Henrique Pinho <henriquegravino@outlook.com>
2024-02-26 12:23:24 -0300

    fix: button and select styles

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -5,14 +5,14 @@
 <template>
   <form @submit.prevent="props.cadastraTarefa">
       <div class="row">
-        <div class="col-md-6">
+        <div class="col-md-6 mt-2 mb-2">
           <input :value="props.tarefaTemp" @change="props.editaTarefaTemp" required class="form-control" type="text" placeholder="O que você precisa fazer?">
         </div>
-        <div class="col-md-2">
-          <button class="btn btn-primary" type="submit">Salvar</button>
+        <div class="col-md-2 d-grid">
+          <button class="btn btn-primary mt-2 mb-2" type="submit">Salvar</button>
         </div>
-        <div class="col-md-4">
-          <select class="form-control" @change="props.trocarFiltro">
+        <div class="col-md-4 mt-2 mb-2">
+          <select class="form-select" @change="props.trocarFiltro">
             <option value="todas">Todas as tarefas</option>
             <option value="pendentes">Tarefas pendentes</option>
             <option value="finalizadas">Taferas finalizadas</option>
