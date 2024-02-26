c8cd438804036bafd041608b6dc1d5ec50cb237e
Henrique Pinho <henriquegravino@outlook.com>
2024-02-26 11:56:18 -0300

    update: html tags made into vue components

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <script setup>
   import { reactive } from 'vue';
+  import Header from './components/Header.vue';
+  import Form from './components/Form.vue';
+  import Tasks from './components/Tasks.vue';
 
   const estado = reactive({
     filtro: 'todas',
@@ -53,34 +56,9 @@
 
 <template>
   <div class="container">
-    <header class="p-5 mt-4 mb-4 bg-light rounded-3">
-      <h1>Minhas tarefas</h1>
-      <p>Você possui {{ getTarefasPendentes().length }} tarefas pendentes</p>
-    </header>
-
-    <form @submit.prevent="cadastraTarefa">
-      <div class="row">
-        <div class="col-md-6">
-          <input :value="estado.tarefaTemp" @change="e => estado.tarefaTemp = e.target.value" required class="form-control" type="text" placeholder="O que você precisa fazer?">
-        </div>
-        <div class="col-md-2">
-          <button class="btn btn-primary" type="submit">Salvar</button>
-        </div>
-        <div class="col-md-4">
-          <select class="form-control" @change="e => estado.filtro = e.target.value">
-            <option value="todas">Todas as tarefas</option>
-            <option value="pendentes">Tarefas pendentes</option>
-            <option value="finalizadas">Taferas finalizadas</option>
-          </select>
-        </div>
-      </div>
-    </form>
-    <ul class="list-group mt-4">
-      <li class="list-group-item" v-for="tarefa in getTarefasFiltradas()">
-        <input @change="e => tarefa.finalizada = e.target.checked" :checked="tarefa.finalizada" :id="tarefa.titulo" type="checkbox">
-        <label :class="{ done: tarefa.finalizada }" class="ms-3" :for="tarefa.titulo">{{ tarefa.titulo }}</label>
-      </li>
-    </ul>
+    <Header :tarefas-pendentes="getTarefasPendentes().length" />
+    <Form :trocar-filtro="e => estado.filtro = e.target.value" :tarefa-temp="estado.tarefaTemp" :edita-tarefa-temp="e => estado.tarefaTemp = e.target.value" :cadastra-tarefa="cadastraTarefa" />
+    <Tasks :tarefas="getTarefasFiltradas()" />
   </div>
 </template>
 
